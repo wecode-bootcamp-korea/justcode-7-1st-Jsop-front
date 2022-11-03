00a5c0439941f4a6e87d8605816c71c454b16b97
kiminSong <kiminsee22@gmail.com>
2022-11-03 17:11:20 +0900

    login ui,기능 진행

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
+import Forgotpassword from './Login/Forgotpassword';
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgotpw" element={<Forgotpassword />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>

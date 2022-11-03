886886e3a83a084ebfd4f5496104ad2fd156f9d0
SulhwaChoi <imsulhwa00@gmail.com>
2022-11-04 07:06:30 +0900

    라우터 설정 변경

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import Router from './pages/Router';
+import App from './App';
 
 import './styles/reset.scss';
 import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(<App />);

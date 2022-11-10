c39a6af3fa3cdca13632e52986e5d7d263551f79
JIWON CHOI <jgb07012@gmail.com>
2022-11-09 22:53:33 -0500

    수정 완료

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -5,6 +5,7 @@ import Product from './Product/Product';
 import MyPage from './MyPage/MyPage';
 import CheckOut from './Checkout/CheckOut';
 import ProductDetail from './ProductDetail/ProductDetail';
+import Recall from '../components/Header/infoBar/Recall';
 function Router() {
   return (
     <BrowserRouter>
@@ -15,6 +16,7 @@ function Router() {
         <Route path="/checkout" element={<CheckOut />} />
         <Route path="/detail" element={<ProductDetail />} />
         <Route path="/detail/:id" element={<ProductDetail />} />
+        <Route path="/recall" element={<Recall />} />
       </Routes>
     </BrowserRouter>
   );
